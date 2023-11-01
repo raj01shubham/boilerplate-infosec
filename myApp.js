94e40461139db426230c485d0bd376bd6dacffae
raj01shubham <raj01shubham@gmail.com>
2023-11-01 16:57:22 +0530

    added HTTP Strict Transport Security (HSTS) method

--- myApp.js
+++ myApp.js
@@ -2,11 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+ninetyDaysInSeconds = 90*24*60*60;
+
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'DENY'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 
 
 
