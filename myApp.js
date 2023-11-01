623da904c22fa39762816f2387ef12a45c46ce84
raj01shubham <raj01shubham@gmail.com>
2023-11-01 16:59:50 +0530

    added line closure

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.use(helmet.frameguard({action: 'DENY'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
 
